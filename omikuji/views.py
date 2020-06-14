14afaffffd0dcd08208c3fb0d470ae6fb0052b31
YoshidaSatoshi <se.yoshida.satoshi@gmail.com>
2020-06-14 21:37:43 +0900

    おみくじ機能の実装

--- omikuji/views.py
+++ omikuji/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random   # randomをインポート
 
 
 # Create your views here.
@@ -7,4 +8,10 @@ def index(request):
 
 
 def result(request):
-    return render(request, 'omikuji/result.html')
+    l = ('大吉', '中吉', '小吉', '凶', '大凶')
+    fortun = random.choice(l)
+    # 運勢の情報を格納
+    context = {
+        'omikuji': fortun
+    }
+    return render(request, 'omikuji/result.html', context)  # contextの情報をテンプレートに送る
